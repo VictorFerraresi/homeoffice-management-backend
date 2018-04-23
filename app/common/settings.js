e553054ffbce576880ebc89fedd664deed629cd8
Victor Ferraresi <victor_ferraresi@hotmail.com>
2018-04-23 15:29:10 -0300

    :arrow_up: Adding NPM dependencies

--- app/common/settings.js
+++ app/common/settings.js
@@ -1,19 +1,19 @@
 module.exports = {
-    name: 'API',
-    env: process.env.NODE_ENV || 'development',
-    port: process.env.PORT || 3000,
-    base_url: process.env.BASE_URL || 'http://localhost:3000',
-    db: {
-        uri: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/api',
-    },
-    logger: {
-        level: 0
-    },
-    prefix: '/api',
-    api:{
-        version: 'v1'
-    },
-    jwt:{
-        secret: 'hobackend123'
-    }
+  name: 'API',
+  env: process.env.NODE_ENV || 'development',
+  port: process.env.PORT || 3000,
+  base_url: process.env.BASE_URL || 'http://localhost:3000',
+  db: {
+    uri: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/api',
+  },
+  logger: {
+    level: 0
+  },
+  prefix: '/api',
+  api:{
+    version: 'v1'
+  },
+  jwt:{
+    secret: 'hobackend123'
+  }
 };
---
+++ app/security/jwt-middleware.js
@@ -0,0 +1,31 @@
+const jsonwebtoken = require('jsonwebtoken');
+const config = require('../common/config');
+
+module.exports = {
+  verifyToken: function(req, res, next) {
+    if(req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT'){
+      jsonwebtoken.verify(req.headers.authorization.split(' ')[1], config.jwt.secret, function(err, decode) {
+        if(err) req.user = undefined;
+        req.user = decode;
+        next();
+      });
+    } else {
+      req.user = undefined;
+      next();
+    }
+  },
+  requireToken: function(req, res, next) {
+    if(req.user) {
+      next();
+    } else {
+      res.status(401);
+      var ret = {
+        error: {
+          code: 401,
+          message: "Usuário não autenticado (JWT)."
+        }
+      };
+      return res.send(ret);
+    }
+  }
+};
