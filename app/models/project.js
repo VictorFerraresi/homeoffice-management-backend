c903844d22f635a7de60090ca9d87e16fa9fd5fa
Victor Ferraresi <victor_ferraresi@hotmail.com>
2018-04-24 14:00:19 -0300

    :books: Added Project database schema

---
+++ app/models/project.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const ProjectSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    index: { unique: true },
+    minlen: 5,
+    maxlen: 128,
+    match: [/^[a-zA-Z0-9]+([_-]?[a-zA-Z0-9])*$/, 'invalid project name']
+  },
+  members: [{ type: Schema.Types.ObjectId, ref: 'User' }]
+}, { timestamps: { createdAt: 'createdAt' } });
+
+class Project {
+
+}
+
+module.exports = () => {
+  ProjectSchema.loadClass(Project);
+  return mongoose.model('Project', ProjectSchema);
+};
