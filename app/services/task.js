46ff5999040e0b90777b248ae1159dd449f21b64
diogosalmeida <diogomean@gmail.com>
2018-04-25 00:58:52 -0300

    create service tasks

---
+++ app/services/task.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const logger = require('../common/logger');
+const Task = mongoose.model('Task');
+
+class TaskService {
+   /**
+   * Method responsible for returning all the Tasks in the database as a RESTful webservice
+   * @param  {Object}   req  HTTP Request
+   * @param  {Object}   res  HTTP Response
+   * @param  {Function} next Next function to be called in the chain
+   */
+    static getAll(req, res, next) {
+        Task.find({}, (err, tasks) => {
+            if (err) {
+                logger.error(err);
+                return next(new errors.InvalidContentError(err.errors.name.message));
+            }
+            res.send(tasks);
+            next();
+        });
+    }
+}
+
+module.exports = TaskService;
