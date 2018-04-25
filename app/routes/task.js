f27afe84276e9647ca54de1d347071301f80beaa
Victor Ferraresi <victor_ferraresi@hotmail.com>
2018-04-25 15:36:48 -0300

    :sparkles: Added a new method to add a user(member) to a Task

--- app/routes/task.js
+++ app/routes/task.js
@@ -9,5 +9,6 @@ routerInstance.get('/tasks/:project_name', TaskService.findByProject);
 routerInstance.post('/tasks', TaskService.createNew);
 routerInstance.post('/tasks/setPriority/', TaskService.setPriority);
 routerInstance.post('/tasks/setStatus/', TaskService.setStatus);
+routerInstance.post('/tasks/addMember/', TaskService.addMember);
 
 module.exports = routerInstance;
