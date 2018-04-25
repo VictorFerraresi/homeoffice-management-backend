e4c8f0ffd75bcd6b752dd9628b89672534e154ed
Victor Ferraresi <victor_ferraresi@hotmail.com>
2018-04-25 10:36:41 -0300

    :recycle: Refactored code to pass eslint and fixed an undef variable error

--- app/models/task.js
+++ app/models/task.js
@@ -3,22 +3,21 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const TaskSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-        index: { unique: true },
-        minlen: 5,
-        maxlen: 128,
-        match: [/^[a-zA-Z0-9]+([_-]?[a-zA-Z0-9])*$/, 'invalid project name']
-    },
-    priority: {
-        type: String,
-        required: true,
-        index: { unique: true },
-    },
-    project: 
-        { type: Schema.Types.ObjectId, ref: 'Project' }
-    ,
+  name: {
+    type: String,
+    required: true,
+    index: { unique: true },
+    minlen: 5,
+    maxlen: 128,
+    match: [/^[a-zA-Z0-9]+([_-]?[a-zA-Z0-9])*$/, 'invalid project name']
+  },
+  priority: {
+    type: String,
+    required: true,
+    index: { unique: true }
+  },
+  project:
+  { type: Schema.Types.ObjectId, ref: 'Project' }
 }, { timestamps: { createdAt: 'createdAt' } });
 
 class Task {
@@ -26,6 +25,6 @@ class Task {
 }
 
 module.exports = () => {
-    TaskSchema.loadClass(Task);
-    return mongoose.model('Task', TaskSchema);
+  TaskSchema.loadClass(Task);
+  return mongoose.model('Task', TaskSchema);
 };
